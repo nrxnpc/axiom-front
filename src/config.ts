40f2af91afe638441b5c96a2b0eb675426ee6aa7
Mark Grama <hello@markgrama.dev>
2025-12-24 00:42:17 +0300

    fix port

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const API_URL = import.meta.env.VITE_API_URL || 'http://80.64.16.58/:8081/api/v1';
+export const API_URL = import.meta.env.VITE_API_URL || 'http://80.64.16.58:8081/api/v1';
 // export const API_URL = import.meta.env.VITE_API_URL || 'http://85.192.28.187:8080/api/v1';
 export const API_KEY = import.meta.env.VITE_API_KEY || 'nsp_admin_api_key_2024';
